5301dc14cc867cf41113f8184caf80752ca58d8c
Amos Egonmwan <amos.egonmwan@hotmail.com>
2024-08-08 11:48:50 -0600

    configured s3 remote backend

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
-provider "aws" {  
-  region = "us-west-2"
+terraform {
+  backend "s3" {
+    bucket = "s3-remote-backend-2024"
+    key = "github-actions/terraform.tfstate"
+    region = "us-west-2"    
+  }
 }
+
+provider "aws" {  
+    region = "us-west-2"
+}
+
 
 resource "aws_vpc" "github-actions" {
     cidr_block = "10.0.0.0/24"
